7946ba1defc4255ec2fa3377d3b174147365a740
JLo'w <Jean-Loup.Beaussart@polytech-lille.net>
2016-01-12 15:37:45 +0100

    Add comments in WinUtil files.

--- LoP_launcher/WinUtil.cpp
+++ LoP_launcher/WinUtil.cpp
@@ -1,3 +1,27 @@
+// Copyright © 2015,2016 Jean-loup Beaussart 
+//
+// This file is part of Light of Paladin Launcher.
+// 
+// Light of Paladin Launcher is free software : you can redistribute it and / or modify
+// it under the terms of the LGPL Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Light of Paladin Launcher is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+// LGPL Lesser General Public License for more details.
+// 
+// You should have received a copy of the LGPL Lesser General Public License
+// along with Light of Paladin Launcher. If not, see < http://www.gnu.org/licenses/ >.
+
+//! \file WinUtil.cpp
+//! \author Jean-loup Beaussart
+//! \version 0.1
+//! \copyright 2015,2016 Jean-loup Beaussart LGPLv3
+//! \brief Utility functions for Windows OS.
+
+
 #include "WinUtil.h"
 #include <utf8.h>
 #include <Windows.h>
